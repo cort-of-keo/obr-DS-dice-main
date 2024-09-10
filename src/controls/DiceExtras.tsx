f81b4d47aab93abe0e70d61700ceed870b138068
cort-of-keo <kolsen8@gmail.com>
2024-09-10 15:14:22 -0700

    Added new button and logic to track Char separate from Bonus

--- src/controls/DiceExtras.tsx
+++ src/controls/DiceExtras.tsx
@@ -6,17 +6,28 @@ import Stack from "@mui/material/Stack";
 import Menu from "@mui/material/Menu";
 import Tooltip from "@mui/material/Tooltip";
 
+import { DieChar } from "./DieChar";
 import { DieBonus } from "./DieBonus";
-import { DieAdvantage } from "./DieAdvantage";
+import { DieDedge } from "./DieDedge";
+import { DieBane } from "./DieBane";
+import { DieEdge } from "./DieEdge";
+import { DieSkill } from "./DieSkill";
 import { useDiceControlsStore } from "./store";
 import { useDiceRollStore } from "../dice/store";
 
 export function DiceExtras() {
   const bonus = useDiceControlsStore((state) => state.diceBonus);
   const setBonus = useDiceControlsStore((state) => state.setDiceBonus);
-  const advantage = useDiceControlsStore((state) => state.diceAdvantage);
-  const setAdvantage = useDiceControlsStore((state) => state.setDiceAdvantage);
-
+  const char = useDiceControlsStore((state) => state.diceChar);
+  const setChar = useDiceControlsStore((state) => state.setDiceChar);
+  const dedge = useDiceControlsStore((state) => state.diceDedge);
+  const setDedge = useDiceControlsStore((state) => state.setDiceDedge);
+  const edge = useDiceControlsStore((state) => state.diceEdge);
+  const setEdge = useDiceControlsStore((state) => state.setDiceEdge);
+  const bane = useDiceControlsStore((state) => state.diceBane);
+  const setBane = useDiceControlsStore((state) => state.setDiceBane);
+  const skill = useDiceControlsStore((state) => state.diceSkill);
+  const setSkill = useDiceControlsStore((state) => state.setDiceSkill);
   const clearRoll = useDiceRollStore((state) => state.clearRoll);
   const roll = useDiceRollStore((state) => state.roll);
   function clearRollIfNeeded() {
@@ -25,7 +36,7 @@ export function DiceExtras() {
     }
   }
 
-  /** Controls (bonus and adv/dis) */
+  /** Controls (char score, double edge/bane, edge/bane, and skilled) */
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
@@ -68,26 +79,74 @@ export function DiceExtras() {
         }}
       >
         <Stack>
-          <DieBonus
+          <DieChar
             bonus={bonus}
+            char={char}
             onChange={(bonus) => {
               setBonus(bonus);
+              setChar(char);
               clearRollIfNeeded();
             }}
             onIncrease={() => {
               setBonus(bonus + 1);
+              setChar(char + 1);
               clearRollIfNeeded();
             }}
             onDecrease={() => {
               setBonus(bonus - 1);
+              setChar(char - 1);
               clearRollIfNeeded();
             }}
           />
           <Divider variant="middle" />
-          <DieAdvantage
-            advantage={advantage}
-            onChange={(advantage) => {
-              setAdvantage(advantage);
+          <DieDedge
+            dedge={dedge}
+            onChange={(dedge) => {
+              setDedge(dedge);
+              clearRollIfNeeded();
+            }}
+          />
+          <Divider variant="middle" />
+          <DieEdge
+            edge={edge}
+            onChange={(edge) => {
+              if (edge === null){
+                setBonus(bonus - 2);
+              }
+              else if (edge === "EDGE"){
+                setBonus(bonus + 2)
+                //setBane(null);
+              }
+              setEdge(edge);
+              clearRollIfNeeded();
+            }}
+          />
+          <Divider variant="middle" />
+          <DieBane
+            bane={bane}
+            onChange={(bane) => {
+              if (bane === null){
+                setBonus(bonus + 2);
+              }
+              else if (bane === "BANE"){
+                setBonus(bonus - 2);
+                //setEdge(null);
+              }
+              setBane(bane);
+              clearRollIfNeeded();
+            }}
+          />
+           <Divider variant="middle" />
+          <DieSkill
+            skill={skill}
+            onChange={(skill) => {
+              if (skill === null ){
+                setBonus(bonus - 2);
+              }
+              else if (skill === "SKILL"){
+                setBonus(bonus + 2);
+              }
+              setSkill(skill);
               clearRollIfNeeded();
             }}
           />
